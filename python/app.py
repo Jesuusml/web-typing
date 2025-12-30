d785f112202372daf00429294406a93841725f16
Jesus ML <madafaking@PC-JML.localdomain>
2025-12-30 01:05:46 +0100

    Python/Flask template

---
+++ python/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello World'
+
+if __name__ == '__main__':
+    PORT = int(os.environ.get('PORT', 3001))
+    app.run(host='0.0.0.0', port=PORT, debug=True)
